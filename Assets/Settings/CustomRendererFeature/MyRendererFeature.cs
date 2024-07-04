314ff69bc025c40f7ea5854eaa4b6378162391ce
Mr.ture.China <mr.ture.china@gmail.com>
2024-07-04 20:16:47 +0800

    添加官方演示的自定义通道

--- Assets/Settings/CustomRendererFeature/MyRendererFeature.cs
+++ Assets/Settings/CustomRendererFeature/MyRendererFeature.cs
@@ -6,32 +6,61 @@ using UnityEngine.Rendering.Universal;
 
 public class MyRendererFeature : ScriptableRendererFeature
 {
-    [SerializeField] private Shader shader;
+    /// <summary>
+    /// 在这个渲染功能里要使用的 Shader，渲染功能就是 Renderer Feature 的官方翻译，可以理解为一个自定义的步骤
+    /// </summary>
+    [SerializeField]
+    private Shader shader;
+
+    /// <summary>
+    /// 材质
+    /// </summary>
     private Material material;
+    /// <summary>
+    /// 红色调渲染通道，实际上和 BiRP 的通道不是一个逻辑，BiRP 的通道顺序依赖于 SubShader，而 URP 的这个基于 ScriptableRendererFeature 的顺序则更依赖于渲染步骤，高于 SubShader
+    /// </summary>
     private RedTintRenderPass redTintRenderPass;
 
+    /// <summary>
+    /// 创建方法
+    /// </summary>
     public override void Create()
     {
         if (shader == null)
         {
             return;
         }
+
+        // 创建材质，然后创建自定义的通道
         material = CoreUtils.CreateEngineMaterial(shader);
         redTintRenderPass = new RedTintRenderPass(material);
         
-        redTintRenderPass.renderPassEvent = RenderPassEvent.AfterRenderingSkybox;
+        // 设定注入点，这里选择了在后期处理之后生效，红色调渲染通道的效果特别霸道适合放到最后面
+        redTintRenderPass.renderPassEvent = RenderPassEvent.AfterRenderingPostProcessing;
     }
 
-    public override void AddRenderPasses(ScriptableRenderer renderer,
-        ref RenderingData renderingData)
+    /// <summary>
+    /// 注入通道方法
+    /// </summary>
+    /// <param name="renderer"></param>
+    /// <param name="renderingData"></param>
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
     {
-        if (renderingData.cameraData.cameraType == CameraType.Game)
+        // 根据摄像机类型注入，这里给游戏摄像机和场景视图注入，没有注入 VR 和其他的一些类型
+        if (renderingData.cameraData.cameraType == CameraType.Game || renderingData.cameraData.cameraType == CameraType.SceneView)
         {
+            // 注入通道
             renderer.EnqueuePass(redTintRenderPass);
         }
     }
+
+    /// <summary>
+    /// 释放方法，或者你要是愿意也可以叫析构函数，反正他的大名叫 Dispose
+    /// </summary>
+    /// <param name="disposing"></param>
     protected override void Dispose(bool disposing)
     {
+        // 销毁材质
         CoreUtils.Destroy(material);
     }
 }
--- Assets/Settings/CustomRendererFeature/RedTintRenderPass.cs
+++ Assets/Settings/CustomRendererFeature/RedTintRenderPass.cs
@@ -3,49 +3,80 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
+/// <summary>
+/// 红色调自定义通道
+/// </summary>
 public class RedTintRenderPass : ScriptableRenderPass
 {
+    /// <summary>
+    /// 材质
+    /// </summary>
     private Material material;
 
+    /// <summary>
+    /// 渲染图片描述符，用于指定管线输入给这个通道的图片的详细要求，用于实现类似 BiRP 的 GrabPass 的功能
+    /// </summary>
     private RenderTextureDescriptor textureDescriptor;
+    /// <summary>
+    /// 渲染图片数据，作为中转存在
+    /// </summary>
     private RTHandle textureHandle;
 
+    /// <summary>
+    /// 构造方法，不是什么特别的内部功能指定的方法
+    /// </summary>
+    /// <param name="material"></param>
     public RedTintRenderPass(Material material)
     {
         this.material = material;
 
-        textureDescriptor = new RenderTextureDescriptor(Screen.width,
-            Screen.height, RenderTextureFormat.Default, 0);
+        // 创建一个渲染图片输出器
+        // 宽高是屏幕宽高，就是全屏输出
+        // 默认颜色格式，这个模式是根据平台变化的，但是它不支持 HDR，也有支持 HDR 的默认颜色格式，但对于这个红色调通道来说没必要
+        // 深度缓冲区的位数为 0，即不要深度缓冲区信息
+        textureDescriptor = new RenderTextureDescriptor(Screen.width, Screen.height, RenderTextureFormat.Default, 0);
     }
 
-    public override void Configure(CommandBuffer cmd,
-        RenderTextureDescriptor cameraTextureDescriptor)
+    /// <summary>
+    /// 配置方法，这个方法会在这个通道正式调用前调用，这个方法不是必须覆写的，根据需要覆写
+    /// </summary>
+    /// <param name="cmd"></param>
+    /// <param name="cameraTextureDescriptor"></param>
+    public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
     {
-        // Set the texture size to be the same as the camera target size.
+        // 设置图片输出的宽高为摄像机的宽高
         textureDescriptor.width = cameraTextureDescriptor.width;
         textureDescriptor.height = cameraTextureDescriptor.height;
 
-        // Check if the descriptor has changed, and reallocate the RTHandle if necessary
+        // 在必要的时候创建一个 RTHandle
+        // 按照文档所述如果没有 RTHandle 或者 RTHandle 发生了需要重新创建的变化则会创建新的 RTHandle
+        // 总是会传递 RTHandle 给 ref 参数
         RenderingUtils.ReAllocateIfNeeded(ref textureHandle, textureDescriptor);
     }
 
-    public override void Execute(ScriptableRenderContext context,
-        ref RenderingData renderingData)
+    /// <summary>
+    /// 执行方法，真正的运行 Pass 进行渲染
+    /// </summary>
+    /// <param name="context"></param>
+    /// <param name="renderingData"></param>
+    public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
     {
-        //Get a CommandBuffer from pool.
+        // 从命令缓冲池里取出命令缓冲
+        // 可以理解为这个命令只要执行了流程就会继续走，我们先做自定义的处理，做完后执行这个命令让渲染继续
         CommandBuffer cmd = CommandBufferPool.Get();
 
-        RTHandle cameraTargetHandle =
-            renderingData.cameraData.renderer.cameraColorTargetHandle;
+        // 取出摄像机的渲染图片
+        RTHandle cameraTargetHandle = renderingData.cameraData.renderer.cameraColorTargetHandle;
 
-        // Blit from the camera target to the temporary render texture,
-        // using the first shader pass.
+        // 将摄像机的图片，使用指定材质的 0 号通道，渲染到中转图片
+        // 在 Shader 里这个 0 号通道是调色后输出颜色
+        // Blit 是一个计算机图形学的词，他指的是从一个图片快速渲染到另一个图片的操作
         Blit(cmd, cameraTargetHandle, textureHandle, material, 0);
-        // Blit from the temporary render texture to the camera target,
-        // using the second shader pass.
+        // 将中转图片，使用指定材质的 1 号通道，渲染到摄像机的图片
+        // 在 Shader 里这个 1 号通道是直接输出颜色
         Blit(cmd, textureHandle, cameraTargetHandle, material, 1);
 
-        //Execute the command buffer and release it back to the pool.
+        // 执行命令，然后释放掉
         context.ExecuteCommandBuffer(cmd);
         CommandBufferPool.Release(cmd);
     }
