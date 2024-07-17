cd70744e70b34ef30b0c3323608cca3ff1afb1d3
Mr.ture.China <mr.ture.china@gmail.com>
2024-07-17 18:43:29 +0800

    导入更新版本的GrabScreen，但是依然不太顺利，需要调整或者对接

--- Assets/RendererSettings/CustomRendererFeature/GrabPass/GrabScreenFeature.cs
+++ Assets/RendererSettings/CustomRendererFeature/GrabPass/GrabScreenFeature.cs
@@ -3,40 +3,70 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.Rendering.Universal.Internal;
 
-[ExecuteInEditMode]
+/// <summary>
+/// 抓取屏幕的自定义渲染功能
+/// </summary>
 public class GrabScreenFeature : ScriptableRendererFeature
 {
+    /// <summary>
+    /// 设置
+    /// </summary>
     [System.Serializable]
     public class Settings
     {
+        /// <summary>
+        /// 输出到的纹理名称，Shader 中通过这个名称获取抓取的纹理
+        /// </summary>
         public string TextureName = "_GrabPassTransparent";
+        /// <summary>
+        /// 层遮罩
+        /// </summary>
         public LayerMask LayerMask;
+        /// <summary>
+        /// 在这个事件发出时执行
+        /// </summary>
+        public RenderPassEvent RenderPassEvent;
+        /// <summary>
+        /// 执行时机偏移，即在这个事件发出时执行的优先级，越小则越靠前
+        /// </summary>
+        public int Offset;
+        /// <summary>
+        /// 传递纹理给这个材质
+        /// </summary>
+        public Material BlitMaterial;
     }
 
+    /// <summary>
+    /// 抓取屏幕的自定义渲染通道
+    /// </summary>
     class GrabPass : ScriptableRenderPass
     {
         RenderTargetHandle tempColorTarget;
+        /// <summary>
+        /// 设置
+        /// </summary>
         Settings settings;
-
-        RenderTargetIdentifier cameraTarget;
 
         public GrabPass(Settings s)
         {
             settings = s;
-            renderPassEvent = RenderPassEvent.AfterRenderingTransparents;
+            renderPassEvent = settings.RenderPassEvent + settings.Offset;
             tempColorTarget.Init(settings.TextureName);
-        }
-
-        public void Setup(RenderTargetIdentifier cameraTarget)
-        {
-            this.cameraTarget = cameraTarget;
         }
 
         public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
         {
-            cmd.GetTemporaryRT(tempColorTarget.id, cameraTextureDescriptor);
+            var descriptor = cameraTextureDescriptor;
+            descriptor.msaaSamples = 1;
+            descriptor.depthBufferBits = 0;
+
+            cmd.GetTemporaryRT(tempColorTarget.id, descriptor);
             cmd.SetGlobalTexture(settings.TextureName, tempColorTarget.Identifier());
+
+            ConfigureTarget(tempColorTarget.Identifier());
+            ConfigureClear(ClearFlag.Color, Color.black);
         }
 
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
@@ -45,7 +75,15 @@ public class GrabScreenFeature : ScriptableRendererFeature
             context.ExecuteCommandBuffer(cmd);
             cmd.Clear();
 
-            Blit(cmd, cameraTarget, tempColorTarget.Identifier());
+            if ((int)settings.RenderPassEvent >= (int)RenderPassEvent.BeforeRenderingPostProcessing)
+            {
+                cmd.Blit(null, tempColorTarget.Identifier(), settings.BlitMaterial, 2);
+            }
+            else
+            {
+                var cameraTarget = renderingData.cameraData.renderer.cameraColorTarget;
+                Blit(cmd, cameraTarget, tempColorTarget.Identifier());
+            }
 
             context.ExecuteCommandBuffer(cmd);
             CommandBufferPool.Release(cmd);
@@ -64,18 +102,42 @@ public class GrabScreenFeature : ScriptableRendererFeature
 
         FilteringSettings m_FilteringSettings;
         RenderStateBlock m_RenderStateBlock;
+        RenderTargetHandle renderTarget;
+        RenderTargetIdentifier depthHandle;
+
+        public RenderTargetIdentifier RenderTarget => renderTarget.Identifier();
 
         public RenderPass(Settings settings)
         {
             this.settings = settings;
-            renderPassEvent = RenderPassEvent.AfterRenderingTransparents + 1;
+            renderPassEvent = settings.RenderPassEvent + settings.Offset + 1;
 
             m_ShaderTagIdList.Add(new ShaderTagId("SRPDefaultUnlit"));
             m_ShaderTagIdList.Add(new ShaderTagId("UniversalForward"));
-            m_ShaderTagIdList.Add(new ShaderTagId("LightweightForward"));
+            m_ShaderTagIdList.Add(new ShaderTagId("UniversalForwardOnly"));
 
             m_FilteringSettings = new FilteringSettings(RenderQueueRange.all, settings.LayerMask);
             m_RenderStateBlock = new RenderStateBlock(RenderStateMask.Nothing);
+
+            renderTarget.Init("_GSF_RenderPass");
+        }
+
+        public void Setup(RenderTargetIdentifier depthHandle)
+        {
+            this.depthHandle = depthHandle;
+        }
+
+        public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
+        {
+            var desc = renderingData.cameraData.cameraTargetDescriptor;
+            desc.msaaSamples = 1;
+            cmd.GetTemporaryRT(renderTarget.id, desc, FilterMode.Point);
+
+            if ((int)settings.RenderPassEvent >= (int)RenderPassEvent.BeforeRenderingPostProcessing)
+            {
+                ConfigureTarget(renderTarget.Identifier(), depthHandle);
+                ConfigureClear(ClearFlag.Color, Color.clear);
+            }
         }
 
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
@@ -85,9 +147,83 @@ public class GrabScreenFeature : ScriptableRendererFeature
             context.ExecuteCommandBuffer(cmd);
             cmd.Clear();
 
-            DrawingSettings drawSettings;
-            drawSettings = CreateDrawingSettings(m_ShaderTagIdList, ref renderingData, SortingCriteria.CommonTransparent);
+            DrawingSettings drawSettings = CreateDrawingSettings(m_ShaderTagIdList, ref renderingData, SortingCriteria.CommonOpaque);
             context.DrawRenderers(renderingData.cullResults, ref drawSettings, ref m_FilteringSettings, ref m_RenderStateBlock);
+
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
+        }
+
+        public override void OnCameraCleanup(CommandBuffer cmd)
+        {
+            cmd.ReleaseTemporaryRT(renderTarget.id);
+        }
+    }
+
+    class CopyDepthPass : ScriptableRenderPass
+    {
+        RenderTargetHandle depthHandle;
+        Settings settings;
+
+        public RenderTargetIdentifier DepthHandle => depthHandle.Identifier();
+
+        public CopyDepthPass(Settings settings)
+        {
+            this.settings = settings;
+
+            depthHandle.Init("_DepthCopy");
+        }
+
+        public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
+        {
+            var desc = renderingData.cameraData.cameraTargetDescriptor;
+            desc.msaaSamples = 1;
+
+            cmd.GetTemporaryRT(depthHandle.id, desc, FilterMode.Point);
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            var cmd = CommandBufferPool.Get("OutlineBlitCMD");
+            context.ExecuteCommandBuffer(cmd);
+            cmd.Clear();
+
+            cmd.Blit(depthHandle.Identifier(), depthHandle.Identifier(), settings.BlitMaterial, 1);
+
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
+        }
+
+        public override void OnCameraCleanup(CommandBuffer cmd)
+        {
+            cmd.ReleaseTemporaryRT(depthHandle.id);
+        }
+    }
+
+    class BlitPass : ScriptableRenderPass
+    {
+        Settings settings;
+        RenderTargetIdentifier renderTarget;
+
+        public BlitPass(Settings settings)
+        {
+            this.settings = settings;
+            renderPassEvent = settings.RenderPassEvent + settings.Offset + 2;
+        }
+
+        public void Setup(RenderTargetIdentifier renderTarget)
+        {
+            this.renderTarget = renderTarget;
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            CommandBuffer cmd = CommandBufferPool.Get();
+            context.ExecuteCommandBuffer(cmd);
+            cmd.Clear();
+
+            var screenTex = colorAttachment;
+            cmd.Blit(null, screenTex, settings.BlitMaterial, 0);
 
             context.ExecuteCommandBuffer(cmd);
             CommandBufferPool.Release(cmd);
@@ -96,19 +232,35 @@ public class GrabScreenFeature : ScriptableRendererFeature
 
     GrabPass grabPass;
     RenderPass renderPass;
-    [SerializeField] Settings settings;
+    BlitPass blitPass;
+    CopyDepthPass copyDepthPass;
+    [SerializeField] Settings settings = new Settings();
 
     public override void Create()
     {
         grabPass = new GrabPass(settings);
         renderPass = new RenderPass(settings);
+        blitPass = new BlitPass(settings);
+        copyDepthPass = new CopyDepthPass(settings);
+        copyDepthPass.renderPassEvent = RenderPassEvent.AfterRenderingPrePasses;
     }
 
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
     {
-        grabPass.Setup(renderer.cameraColorTarget);
+        if ((int)settings.RenderPassEvent >= (int)RenderPassEvent.BeforeRenderingPostProcessing)
+        {
+            renderer.EnqueuePass(copyDepthPass);
+        }
 
         renderer.EnqueuePass(grabPass);
+
+        renderPass.Setup(copyDepthPass.DepthHandle);
         renderer.EnqueuePass(renderPass);
+
+        if ((int)settings.RenderPassEvent >= (int)RenderPassEvent.BeforeRenderingPostProcessing)
+        {
+            blitPass.Setup(renderPass.RenderTarget);
+            renderer.EnqueuePass(blitPass);
+        }
     }
 }
