57a08b6b90bb2c5c3388a03b1688e72dbfcff942
Mr.ture.China <mr.ture.china@gmail.com>
2024-10-14 18:57:40 +0800

    一个描边和一个像素化的滤镜，效果一般但看起来思路没什么问题

--- Assets/Practice/Common/ShaderGraphNode/EdgeDetectionOutlinesInclude.hlsl
+++ Assets/Practice/Common/ShaderGraphNode/EdgeDetectionOutlinesInclude.hlsl
@@ -47,10 +47,12 @@ static float sobelYMatrix[9] = {
     -1, -2, -1
 };
 
+/// 使用 Sobel 算子的深度检测
 void DepthSobel_float(float2 UV, float Thickness, out float Out) {
     float2 sobel = 0;
 
-    [unroll] for(int i = 0; i < 9; i++){
+    [unroll] // 这个注解的意思是展开循环，这会让编译器在生成时把这个循环视为写了九遍的代码而不是一个九次的循环，这样可以省下循环本身消耗的一点点计算量
+    for(int i = 0; i < 9; i++){
         // 卷积的一步，原理是取出这一步的 UV 偏移量乘上步长得到这个采样点在深度图上的 UV，然后使用 URP 提供的宏取出这个位置的深度
         float depth = SHADERGRAPH_SAMPLE_SCENE_DEPTH(UV + sobelSimplePoints[i] * Thickness);
         // 确认深度后一次对两个算子矩阵进行运算，分别存入到 float2 的两个值里
@@ -59,6 +61,51 @@ void DepthSobel_float(float2 UV, float Thickness, out float Out) {
 
     // 因为卷积把 XY 两个轴的检测一起存起来了，这里可以直接取向量长度，长度不会是负数能省下来取绝对值的计算量
     // 但是相对的，取长度本身的计算量也不低
+    Out = length(sobel);
+}
+
+/// 使用 Sobel 算子的颜色检测
+void ColorSobel_float(float2 UV, float Thickness, out float Out) {
+    float2 sobelR = 0;
+    float2 sobelG = 0;
+    float2 sobelB = 0;
+
+    [unroll] // 展开循环，虽然循环本身占用的计算量比起算法本身只有很小一点，但加一个注解不费力顺带能省则省了
+    for(int i = 0; i < 9; i++){
+        // 取出颜色
+        // SHADERGRAPH_SAMPLE_SCENE_COLOR 这个方法有问题，他只有在 Shader Graph 中使用了 SceneColor 节点并且最终指向了颜色输出时才有效
+        float3 rgb = SHADERGRAPH_SAMPLE_SCENE_COLOR(UV + sobelSimplePoints[i] * Thickness);
+
+        // 卷积核，就是两个算子拼一起，因为颜色检测对 RGB 三色各进行一次检测，卷积核提前计算好存下来要比每次都临时计算节约计算量
+        float2 kernel = float2(sobelXMatrix[i], sobelYMatrix[i]);
+
+        // 对三个颜色分别计算
+        sobelR += rgb.r * kernel;
+        sobelG += rgb.g * kernel;
+        sobelB += rgb.b * kernel;
+    }
+
+    // 取三个颜色中计算结果最大的那个，三原色任何一个产生巨大变化都会导致明显的视觉差异，直接取最大的而不是总和，这样还能降低输出的范围防止范围过大加大精细调整时的难度
+    Out = max(length(sobelR), max(length(sobelG), length(sobelB)));
+}
+
+/// 使用 Sobel 算子的亮度检测
+void LuminanceSobel_float(float2 UV, float Thickness, out float Out) {
+    float2 sobel = 0;
+
+    [unroll] // 展开循环，虽然循环本身占用的计算量比起算法本身只有很小一点，但加一个注解不费力顺带能省则省了
+    for(int i = 0; i < 9; i++){
+        // 取出颜色
+        float3 rgb = SHADERGRAPH_SAMPLE_SCENE_COLOR(UV + sobelSimplePoints[i] * Thickness);
+
+        // 卷积核，就是两个算子拼一起，因为颜色检测对 RGB 三色各进行一次检测，卷积核提前计算好存下来要比每次都临时计算节约计算量
+        float luminance = Luminance(rgb);
+
+        // 对三个颜色分别计算
+        sobel += luminance * float2(sobelXMatrix[i], sobelYMatrix[i]);
+    }
+
+    // 取三个颜色中计算结果最大的那个，三原色任何一个产生巨大变化都会导致明显的视觉差异，直接取最大的而不是总和，这样还能降低输出的范围防止范围过大加大精细调整时的难度
     Out = length(sobel);
 }
 
